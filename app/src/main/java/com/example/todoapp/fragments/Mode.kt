38e9b58a3177770b0b82f5fc34d3b2e4a8a5b5f7
admin <mariavasina@inbox.ru>
2023-07-13 23:29:43 +0300

    night theme, colors and styles

---
+++ app/src/main/java/com/example/todoapp/fragments/Mode.kt
@@ -0,0 +1,7 @@
+package com.example.todoapp.fragments
+
+enum class Mode {
+    NIGHT,
+    LIGHT,
+    SYSTEM
+}
---
+++ app/src/main/java/com/example/todoapp/fragments/SettingsFragment.kt
@@ -0,0 +1,17 @@
+package com.example.todoapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentSettingsBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+}
